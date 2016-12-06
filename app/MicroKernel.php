b33b1d90e9639a9c6d00811914accc6d28c1b7af
Stefan Gasser <stefangasser.info@gmail.com>
2016-12-06 22:02:17 +0100

    Add services.yml

--- app/MicroKernel.php
+++ app/MicroKernel.php
@@ -41,6 +41,7 @@ class MicroKernel extends Kernel
     protected function configureContainer(ContainerBuilder $c, LoaderInterface $loader)
     {
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load(__DIR__.'/config/services.yml');
     }
 
     public function getRootDir()
